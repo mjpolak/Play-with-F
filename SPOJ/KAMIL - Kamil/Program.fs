a2be3d1103cf7b4a84ae912a9c9f5fafba97d33b
mjpolak <marcin.jakub.polak@gmail.com>
2017-01-17 14:24:25 +0100

    Kamil AC

---
+++ SPOJ/KAMIL - Kamil/Program.fs
@@ -0,0 +1,7 @@
+﻿open System;
+[<EntryPoint>]
+let m a= 
+    for c=1 to 10 do
+        let d = Console.ReadLine().ToCharArray()
+        printf "%i\n" (Array.fold (fun a x-> match x with |'T'|'D'|'L'|'F' -> 2*a | _ -> a) 1 d)
+    0
